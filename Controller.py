75cdb8fe1bf23da734dc85a4d7654c047c6d37ef
BENULL <327137362@qq.com>
2021-04-09 19:43:51 +0800

    捕获视频bug

--- Controller.py
+++ Controller.py
@@ -1,6 +1,5 @@
 # coding = utf-8
 
-import requests
 from typing import *
 import base64
 import json
@@ -9,14 +8,18 @@ import multiprocessing
 from multiprocessing import Queue
 import queue
 import cv2
-import traceback
 import threading
 import time
+import grequests
 
 
 class Controller:
 
-    __RECOGNIZE_ACTION_URL = 'http://10.176.54.14:55000/recognizeAction'
+    # __RECOGNIZE_ACTION_URL = 'http://10.176.54.14:55000/recognizeAction'
+
+    __RECOGNIZE_ACTION_URLS =['http://10.176.54.14:55000/recognizeAction',
+                              'http://10.176.54.22:21602/recognizeAction']
+
     __ACTION_LABEL = ['站', '坐', '走', '吃', '红绳操', '毛巾操', '未知动作']
 
     __QUEUE_GET_TIMEOUT = 2
@@ -26,45 +29,48 @@ class Controller:
     def __init__(self):
         self.waitingQueueDict = dict()
         self.responseQueue = Queue()
-        self.__cameras = []
+        self.cameras = []
         self.frameCnt = 0
 
         # self.times = 0
         # self.duration = 0
 
     def start(self):
+
         for i in range(1):
             self.procVideo(f'/Users/benull/Downloads/{i}.MOV')
-        # 线程
+
         self.startProcRecognize()
 
 
     def startProcRecognize(self):
 
-        p = multiprocessing.Process(target=self.procRecognizeQueue,
-                                    args=(self.waitingQueueDict, self.responseQueue,))
+        p = multiprocessing.Process(target=self.procRecognizeQueue,args=(self.waitingQueueDict, self.responseQueue,))
         p.start()
 
-        # t = threading.Thread(target=self.procRecognizeQueue, name='procRecognizeThread',
-        #                      args=(self.waitingQueueDict, self.responseQueue,))
-        # t.start()
-
     def __requestRecognizeAction(self, imagesData):
-        param = self.__buildRecognizeParam(imagesData, False, False)
+
+        imageListPerCamera = self.__buildImageListPerCamera(imagesData)
+
+        params = [self.__buildRecognizeParam(imageList, False, False)  for imageList in imageListPerCamera]
         try:
             # start = time.time()
 
-            responseData = self.recognizeAction(param)
+            responseData = self.recognizeAction(params)
 
             # self.duration += (time.time() - start)
             # self.times += 1
             # print(f'{self.times}次请求平均消耗时间{self.duration/self.times*1000}ms')
 
-
             return list(map(self.__procResponseData, imagesData, responseData))
+
         except Exception as e:
-            # traceback.print_exc()
             return []
+
+    def __buildImageListPerCamera(self, imagesData):
+        serverNum = len(Controller.__RECOGNIZE_ACTION_URLS)
+        return [list(filter(None,imagesData[i::serverNum])) for i in range(serverNum)]
+
 
     def procRecognizeQueue(self, waitingQueueDict, responseQueue):
         while True:
@@ -72,21 +78,22 @@ class Controller:
             if not imagesData:
                 continue
             if needRecognize:
-                # pass
-
                 responseQueue.put(self.__requestRecognizeAction(imagesData))
             else:
                 responseQueue.put(list(map(self.__procResponseData, imagesData)))
 
     def __gainFramePerVideo(self, waitingQueueDict):
-
         imagesData = []
         needRecognize = self.frameCnt % Controller.__RECOGNIZE_PER_FRAME == 0
-        for camera in list(waitingQueueDict.keys()):
+        for camera in self.cameras:
+            if camera not in waitingQueueDict:
+                imagesData.append(None)
+                continue
             try:
                 element = waitingQueueDict[camera].get(timeout=Controller.__QUEUE_GET_TIMEOUT)
                 if element == 'DONE':
                     del waitingQueueDict[camera]
+                    imagesData.append(None)
                 else:
                     imagesData.append(element)
             except queue.Empty:
@@ -98,39 +105,52 @@ class Controller:
     def __procResponseData(self, origin, response=dict()):
         camera, frameNum, image = origin
         label = Controller.__ACTION_LABEL[response['personInfo'][0]['action']] if response.get('personInfo') else None
-        return dict(camera=str(self.__cameras.index(camera)), frameNum=frameNum, image=image, label=label)
+        return dict(camera=str(self.cameras.index(camera)), frameNum=frameNum, image=image, label=label)
 
     def __buildRecognizeParam(self, images, pose: bool = False, box: bool = False):
+        if not images: return None
         images = list(map(
             lambda image: {'camera': image[0], 'image': base64.b64encode(cv2.imencode('.jpg', image[2])[1]).decode()},
             images))
         option = dict(pose=pose, box=box)
         return dict(images=images, option=option)
 
-    def procVideo(self, camera: str):
+    def procVideo(self, camera):
         videoCapture = VideoCapture(camera)
-        self.__cameras.append(camera)
+        self.cameras.append(camera)
         self.waitingQueueDict[camera] = Queue(maxsize=15)
         p = multiprocessing.Process(target=videoCapture.captureFrame, args=(self.waitingQueueDict[camera],))
         p.start()
 
-    def recognizeAction(self, param: dict):
+    def recognizeAction(self, params):
         # import os
         # print(f'{os.getpid()} recog ')
-
         import time
         print(f'{time.time()}  startRe')
 
-        response = requests.post(Controller.__RECOGNIZE_ACTION_URL, data=json.dumps(param))
+        requestList = self.__buildRequestList(params)
+
+        responseList = pgrequests.map(requestList)
+
         print(f'{time.time()}  endRe')
-        if response.status_code == 200:
-            res = response.json()
-            if res.get('status') == 0:
-                return res.get('data')
+
+        return self.__processMultiResponse(responseList)
+
+    def __processMultiResponse(self,responseList):
+        mergedData = []
+        for response in responseList:
+            if response.status_code == 200:
+                res = response.json()
+                if res.get('status') == 0:
+                    mergedData.extend(res.get('data'))
+                else:
+                    raise Exception('Recognize Error: ' + res.get('message'))
             else:
-                raise Exception('Recognize Error: ' + res.get('message'))
-        else:
-            raise response.raise_for_status()
+                raise response.raise_for_status()
+        return mergedData
+
+    def __buildRequestList(self,params):
+        return [grequests.post(url,data = json.dumps(param)) for url,param in zip(Controller.__RECOGNIZE_ACTION_URLS,params) if param]
 
     def recv(self):
         res = []
@@ -144,8 +164,9 @@ class Controller:
 if __name__ == '__main__':
     controller = Controller()
 
-    controller.startProcRecognize()
     controller.procVideo('/Users/benull/Downloads/1.MOV')
+    controller.startProcRecognize()
+
 
     import time
 
